a18e1dfc910817cb7e8f3272b1ec22ca31417996
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-21 02:47:15 -0400

    feat: Basic assembly emission + add libs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+use std::{fs::File, path::{Path, PathBuf}};
 
 use clap::Parser;
+use faerie::{ArtifactBuilder, Link, Decl};
+use target_lexicon::triple;
+use iced_x86::code_asm::*;
+use std::str::FromStr;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about)]
@@ -34,4 +38,42 @@ fn main() {
     let args = CLIArgs::parse();
 
     println!("{:?}", args);
+
+    let mut asm = CodeAssembler::new(64).unwrap();
+
+    let mut label_main = asm.create_label();
+    let mut label_add = asm.create_label();
+
+    asm.set_label(&mut label_main).unwrap();
+    asm.mov(rax, 69u64).unwrap();
+    asm.call(label_add).unwrap();
+    asm.ret().unwrap();
+
+    asm.set_label(&mut label_add).unwrap();
+    asm.add(rax, 10).unwrap();
+    asm.ret().unwrap();
+
+    let result = asm.assemble_options(0x0,4).unwrap();
+
+    let main_ip = result.label_ip(&label_main).unwrap() as usize;
+    let add_ip = result.label_ip(&label_add).unwrap() as usize;
+    println!("main: {}, add: {}", main_ip, add_ip);
+
+    let bytes = result.inner.code_buffer;
+    let main = &bytes[main_ip..add_ip];
+    let add = &bytes[add_ip..];
+
+    let filename = "a.o";
+    let f = File::create(Path::new(filename)).unwrap();
+    let mut obj = ArtifactBuilder
+        ::new(triple!("x86_64-unknown-unknown-unknown-macho"))
+    .name(filename.to_owned())
+    .finish();
+
+    obj.declare_with("main", Decl::function().global(), main.to_owned())
+        .unwrap();
+    obj.declare_with("add", Decl::function().global(), add.to_owned())
+        .unwrap();
+
+    obj.write(f).unwrap();
 }
