4dee78ddd71fb2f05c0c5d34308c81468f253b40
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-21 00:43:22 -0400

    feat: Basic CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct CLIArgs {
+    /// The .c files to compile
+    #[arg(required=true, num_args=1..)]
+    files: Vec<PathBuf>,
+
+    /// Compile without linking to an executable
+    #[arg(short)]
+    compile_only: bool,
+
+    /// Output file path
+    #[arg(short, value_name = "PATH")]
+    output: Option<PathBuf>,
+
+    /// Verbose logging output
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Define a macro for the preprocessor
+    #[arg(short = 'D', value_name = "MACRO")]
+    defines: Vec<String>,
+
+    /// Include the given directory in the include search path
+    #[arg(short = 'I', value_name = "PATH")]
+    includes: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CLIArgs::parse();
+
+    println!("{:?}", args);
 }
