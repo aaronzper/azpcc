25e6d5baca4ba3cdc8d8a7834ace669e0602c81b
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-23 02:26:26 -0400

    minor: Get clippy to stfu

--- src/codegen/x86_64/mod.rs
+++ src/codegen/x86_64/mod.rs
@@ -1,6 +1,6 @@
-use std::{path::Path};
+use std::path::Path;
 use target_lexicon::Triple;
 
-pub fn generate(triple: Triple, output: &Path) {
+pub fn generate(_triple: Triple, _output: &Path) {
     println!("Code!");
 }
--- src/preprocessor/directive.rs
+++ src/preprocessor/directive.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 #[derive(Debug)]
+#[allow(dead_code)] // TODO: Remove. Just put for now so clippy shuts up
 pub enum Directive {
     IncludeGlobal(PathBuf),
     IncludeLocal(PathBuf),
