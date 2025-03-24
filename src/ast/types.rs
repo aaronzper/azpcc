443fb70170496e72a2109034c02c2aac4db63221
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-24 17:06:51 -0400

    feat: Helper funcs for Type

--- src/ast/types.rs
+++ src/ast/types.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Type {
     Uint8, // unsigned char
     Uint16,// unsigned short
@@ -21,8 +21,36 @@ pub enum Type {
     // TODO: Structs, enums
 }
 
-#[derive(Debug, Clone)]
+impl Type {
+    pub fn is_integer(&self) -> bool {
+        match self {
+            Self::Uint8 | Self::Uint16 | Self::Uint32 | Self::Uint64 |
+            Self::Int8  | Self::Int16  | Self::Int32  | Self::Int64 => true,
+
+            _ => false
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq)]
 pub struct FunctionType {
     pub return_type: Option<Type>,
     pub args: Box<[(String, Type)]>,
+}
+
+impl PartialEq for FunctionType {
+    fn eq(&self, other: &Self) -> bool {
+        if self.return_type != other.return_type {
+            return false;
+        }
+
+        // Doesnt matter if arg names match, only types
+        for ((_, t1), (_, t2)) in self.args.iter().zip(&other.args) {
+            if t1 != t2 {
+                return false;
+            }
+        }
+
+        true
+    }
 }
