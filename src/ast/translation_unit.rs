fc6f4ad0789a890b25f60f75c0f4bab025f983b7
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-23 20:09:24 -0400

    refactor: Drop Path from translation unit for now

--- src/ast/translation_unit.rs
+++ src/ast/translation_unit.rs
@@ -1,9 +1,6 @@
-use std::path::Path;
-
 use super::Declaration;
 
 #[derive(Debug)]
-pub struct TranslationUnit<'a> {
+pub struct TranslationUnit {
     pub declarations: Box<[Declaration]>,
-    pub file: &'a Path, // Might just make a PathBuf
 }
