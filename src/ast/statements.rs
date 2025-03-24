aaa59d78a9da7a5b0b00898e3057f80b1cddaefd
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-24 17:25:04 -0400

    feat: Verification for statements

--- src/ast/statements.rs
+++ src/ast/statements.rs
@@ -26,16 +26,94 @@ pub struct IfStatement {
     pub else_block: Option<Statement>,
 }
 
+impl IfStatement {
+    fn verify(&self, context: &mut Context) -> Result<(), CompilerError> {
+        if !self.condition.verify(context)?.is_integer() {
+            return Err(CompilerError::SemanticError("If condition must resolve to an integer type"));
+        }
+
+        self.if_block.verify(context)?;
+
+        if let Some(else_stmt) = &self.else_block {
+            else_stmt.verify(context)?;
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
 pub struct WhileLoopStatement {
     pub condition: Expression,
     pub body: Statement,
 }
 
+impl WhileLoopStatement {
+    fn verify(&self, context: &mut Context) -> Result<(), CompilerError> {
+        if !self.condition.verify(context)?.is_integer() {
+            return Err(CompilerError::SemanticError("While condition must resolve to an integer type"));
+        }
+
+        self.body.verify(context)?;
+
+        Ok(())
+    }
+}
+
+fn verify_compound(stmts: &Box<[Statement]>, context: &mut Context) ->
+    Result<(), CompilerError> {
+
+    let mut inner = context.inner();
+
+    for stmt in stmts {
+        stmt.verify(&mut inner)?;
+    }
+
+    Ok(())
+}
+
+fn verify_return(expr: &Option<Expression>, context: &mut Context) ->
+    Result<(), CompilerError> {
+
+    let expected_type = context
+        .return_type()
+        .ok_or(CompilerError::SemanticError("Cannot return from outside of a function"))?
+        .cloned();
+
+    match expr {
+        Some(ex) => {
+            let actual_type = ex.verify(context)?;
+
+            if let Some(t) = expected_type {
+                if t != actual_type {
+                    return Err(CompilerError::SemanticError("Tried to return incorrect type"));
+                }
+            } else {
+                return Err(CompilerError::SemanticError("Cannot return value from void fn"));
+            }
+        },
+
+        None => {
+            if expected_type.is_some() {
+                return Err(CompilerError::SemanticError("Cannot return void from non-void fn"));
+            }
+        },
+    }
+
+    Ok(())
+}
+
 impl Statement {
     pub fn verify(&self, context: &mut Context) -> Result<(), CompilerError> {
+        match self {
+            Self::Compound(stmts) => verify_compound(stmts, context)?,
+            Self::Declaration(decl) => decl.verify(context)?,
+            Self::Expression(expr) => { expr.verify(context)?; },
+            Self::If(x) => x.verify(context)?,
+            Self::WhileLoop(x) => x.verify(context)?,
+            Self::Return(expr) => verify_return(expr.as_ref(), context)?,
+        };
 
-        // TODO
         Ok(())
     }
 }
