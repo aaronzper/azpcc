d12426cab129ddae2beacc60640505a772c4a4d1
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-26 23:21:23 -0400

    feat: Dereference

--- src/codegen/x86_64/expression.rs
+++ src/codegen/x86_64/expression.rs
@@ -146,6 +146,15 @@ impl GeneratorInstance {
                 }
             }
 
+            Expression::Dereference(expr) => {
+                let a = self.gen_expr(&expr.expr)?;
+
+                let instr = Instr::Mov(a.reg.to_string(), format!("[{}]", a.reg));
+                self.add_instr(instr);
+
+                Ok(a)
+            }
+
             Expression::Identifier(id) => {
                 let (sym, type_of) = self.get_symbol(&id).expect("Undefined");
                 let scratch = self.alloc_scratch(get_size(type_of))?;
