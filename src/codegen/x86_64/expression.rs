cb992c631be0b483d5a68d159c7b8e5fe8769b36
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-03-26 22:27:06 -0400

    feat: Modulo

--- src/codegen/x86_64/expression.rs
+++ src/codegen/x86_64/expression.rs
@@ -81,6 +81,31 @@ impl GeneratorInstance {
 
                 Ok(a)
             },
+            
+            Expression::Modulo(args) => {
+                let (a, b) = self.get_binary_scratches(args)?;
+
+                let rax = SizedRegister { reg: Register::Rax, size: a.reg.size }
+                    .to_string();
+                
+                let rdx = SizedRegister { reg: Register::Rdx, size: a.reg.size }
+                    .to_string();
+
+                let instrs = [
+                    Instr::Push("RDX".to_string()),
+                    Instr::Mov(rax.clone(), a.reg.to_string()),
+                    Instr::Cqo,
+                    Instr::Idiv(b.reg.to_string()),
+                    Instr::Mov(a.reg.to_string(), rdx),
+                    Instr::Pop("RDX".to_string()),
+                ];
+
+                for instr in instrs {
+                    self.add_instr(instr);
+                }
+
+                Ok(a)
+            },
 
             Expression::Identifier(id) => {
                 let (sym, type_of) = self.get_symbol(&id).expect("Undefined");
